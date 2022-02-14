353729eb60c3141ebb6b904b4893bb0ac02da93c
Marcio Machado Pontes <marcinhojazz@gmail.com>
2022-02-13 23:07:44 -0300

    update

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -27,7 +27,7 @@ export const Nav = ({}: NavProps) => {
         </div>
       </div>
       <button className="btn btn-square btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">           
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">           
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>               
         </svg>
       </button>
