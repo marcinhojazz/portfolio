a9d4dcc1ad24b5a7b8db5d2db73714f9cb038dc3
Marcio Machado Pontes <marcinhojazz@gmail.com>
2022-02-15 20:05:22 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react'
-import { Button } from '../components/Button'
 import { Nav } from '../components/Nav'
 import { Hero } from '../components/Hero'
 import { About } from '../components/About'
 import { Footer } from '../components/Footer'
-import { Contact } from '../components/Contact'
-import { UpSite } from '../components/UpSite'
 
 const Home = () => {
 	return (
 		<div className='w-full'>
 			<Nav />
 			<Hero />
-			{/* <About /> */}
-			{/* <Contact /> */}
 			<Footer />
 		</div>
 
