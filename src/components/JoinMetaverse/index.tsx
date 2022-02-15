356707b9f6bc98d4a090565911f79eb7f50e4cc8
Marcio Machado Pontes <marcinhojazz@gmail.com>
2022-02-15 11:25:23 -0300

    create button join metaverse office

---
+++ src/components/JoinMetaverse/index.tsx
@@ -0,0 +1,9 @@
+
+export const JoinMetaverse = () => {
+  return (
+    <button className="hover:text-secondary hover:border-secondary btn rounded-full border-primary gap-2 mr-8 font-extralight">
+      <div className="bg-green-on rounded-full w-2 h-2 font-thin"></div>
+      <span>Join now in my metaverse office</span>
+    </button>
+  )
+}
