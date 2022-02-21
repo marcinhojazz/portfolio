f355543f747b973857c4b6eb25ce55571e2bf3c2
Marcio Machado Pontes <marcinhojazz@gmail.com>
2022-02-21 11:37:25 -0300

    gtag setup insert

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,8 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag'
+import Analytics from '../components/Analytics';
+
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../styles/global';
@@ -5,12 +10,26 @@ import '../styles/globals.css';
 import { theme } from '../styles/theme';
 
 
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routerChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+    
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
+        <Analytics />
       </ThemeProvider>
     </>
   )
