8e10ef644a708e05a8505582c4a25b1f3806f211
Marcio Machado Pontes <marcinhojazz@gmail.com>
2022-02-21 13:44:24 -0300

    update config setup g analytics

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = 'G-T5B8WR5Y77'
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import Script from 'next/script';
 import { useRouter } from 'next/router';
 import * as gtag from '../lib/gtag'
 
@@ -7,7 +8,6 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../styles/global';
 import '../styles/globals.css';
 import { theme } from '../styles/theme';
-import Script from 'next/script';
 
 
 
@@ -28,7 +28,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <ThemeProvider theme={theme}>
         <Script 
           strategy='lazyOnload'
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
         />
 
         <Script strategy='lazyOnload'>
@@ -37,7 +37,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+            gtag('config', ${process.env.NEXT_PUBLIC_GA_ID});
           `}
         </Script>
         
